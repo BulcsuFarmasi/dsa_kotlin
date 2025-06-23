892ac85799ce52ab8aa745b114b5608d5ce18dcf
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-06-23 19:59:37 +0200

    understanding the problem

---
+++ src/main/kotlin/s4problemsolvingapproach/UnderstadingTheProblem.kt
@@ -0,0 +1,16 @@
+package s4problemsolvingapproach
+
+/// 1. Can I restate the problem in my own words?
+/// implement addition
+
+/// 2. What are the inputs that go into the problem?
+///
+
+/// 3. What are the outputs that should come from the solution to the problem?
+///
+
+/// 4. Can the outputs be determined from the outputs? In other words, do I have enough information to solve the problem
+///
+
+/// 5. How should I label the important pieces of data that are part of the problem?
+///
