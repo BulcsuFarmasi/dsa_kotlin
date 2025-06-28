c711dc9a5e1ae32e1a4f810db6ffbeeac136d136
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-06-28 20:27:49 +0200

    add basic loop

--- src/main/kotlin/s4problemsolvingapproach/BreakItDown.kt
+++ src/main/kotlin/s4problemsolvingapproach/BreakItDown.kt
@@ -2,6 +2,8 @@ package s4problemsolvingapproach
 
 fun charCount(str: String): Map<String, Int> {
     // make object to return at end
-    // loop over string
+    // loop over string for each character
+    // if char is a key in object, add one to count
+    // if char is not in object add it and set value to 1
     // return object at end
 }
