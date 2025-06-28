1eec7d634f8c1ed21e2dfb2b2e8833fd9dc62d94
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-06-28 20:31:13 +0200

    add char check

--- src/main/kotlin/s4problemsolvingapproach/BreakItDown.kt
+++ src/main/kotlin/s4problemsolvingapproach/BreakItDown.kt
@@ -3,7 +3,8 @@ package s4problemsolvingapproach
 fun charCount(str: String): Map<String, Int> {
     // make object to return at end
     // loop over string for each character
-    // if char is a key in object, add one to count
-    // if char is not in object add it and set value to 1
+    // if char is a number/letter AND a key in object, add one to count
+    // if char is number/letter AND not in object add it to object and set value to 1
+    /// if char is something else (space, period, etc.) don't do anything
     // return object at end
 }
