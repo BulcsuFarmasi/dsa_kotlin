fba7a205d89ab81358560b45b7d01937010a810a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-07-05 22:20:20 +0200

    questions after solving

---
+++ src/main/kotlin/s4problemsolvingapproach/LookBackAndRefactor.kt
@@ -0,0 +1,9 @@
+package s4problemsolvingapproach
+
+/// Can you check your result?
+/// Can you derive the result differently?
+/// Can you understand it at a glance?
+/// Can you use the result or method for some other problem?
+/// Can you improve the performance of your solution?
+/// Can you think of other ways to refactor?
+/// How other people solved this problem?
