57c9130164c2bf98f21fad5738e297623d4ba79c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-07-05 21:56:41 +0200

    basic solution

--- src/main/kotlin/s4problemsolvingapproach/ConcreteExamples.kt
+++ src/main/kotlin/s4problemsolvingapproach/ConcreteExamples.kt
@@ -2,9 +2,9 @@ package s4problemsolvingapproach
 
 /// Write a function which takes in a string and returns counts of each character in the string
 
-charCount("aaaa") // {a:4, b:0, c:0}
-charCount("hello") // {h:1, e:1, l:2, o:1}
-
-"my phone number is 182763"
-"Hello hi"
-charCount("")
+//charCount("aaaa") // {a:4, b:0, c:0}
+//charCount("hello") // {h:1, e:1, l:2, o:1}
+//
+//"my phone number is 182763"
+//"Hello hi"
+//charCount("")
--- src/main/kotlin/s4problemsolvingapproach/SolveOrSimplify.kt
+++ src/main/kotlin/s4problemsolvingapproach/SolveOrSimplify.kt
@@ -1,11 +1,24 @@
 package s4problemsolvingapproach
 
-fun charCount(str: String): Map<String, Int> {
+fun charCount(str: String): HashMap<Char, Int> {
     // make object to return at end
+    var result: HashMap<Char, Int> = hashMapOf(Pair('B', 1));
     // loop over string for each character
+    for (char in str) {
+        if (result[char] != null) {
+            result[char] = result[char]!! + 1
+        } else {
+            result[char] = 1
+        }
+    }
     // if char is a number/letter AND a key in object, add one to count
     // if char is number/letter AND not in object add it to object and set value to 1
     /// if char is something else (space, period, etc.) don't do anything
     // return object at end
+    return result
+}
+
+fun main() {
+    println(charCount("Benedek"))
 }
 
