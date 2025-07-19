db516920e840c9d973486f22eb01584fef43a259
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-07-19 23:17:46 +0200

    solve and simplify and look back and refactor

--- src/main/kotlin/s4problemsolvingapproach/LookBackAndRefactor.kt
+++ src/main/kotlin/s4problemsolvingapproach/LookBackAndRefactor.kt
@@ -7,3 +7,25 @@ package s4problemsolvingapproach
 /// Can you improve the performance of your solution?
 /// Can you think of other ways to refactor?
 /// How other people solved this problem?
+
+fun charCount(str: String): Map<Char, Int> {
+    // make object to return at end
+    val result: MutableMap<Char, Int> = mutableMapOf()
+    // loop over string for each character
+    for (char in str.lowercase()) {
+
+        if (char.isLetterOrDigit()) {
+            result[char] = result.getOrDefault(char, 0) + 1
+        }
+
+    }
+    // if char is a number/letter AND a key in object, add one to count
+    // if char is number/letter AND not in object add it to object and set value to 1
+    /// if char is something else (space, period, etc.) don't do anything
+    // return object at end
+    return result
+}
+
+fun main() {
+    println(charCount("Farmasi Bulcsú"))
+}
--- src/main/kotlin/s4problemsolvingapproach/SolveOrSimplify.kt
+++ src/main/kotlin/s4problemsolvingapproach/SolveOrSimplify.kt
@@ -1,25 +1,25 @@
 package s4problemsolvingapproach
 
-fun charCount(str: String): HashMap<Char, Int> {
-    // make object to return at end
-    val result: HashMap<Char, Int> = hashMapOf()
-    // loop over string for each character
-    for (i in str.indices) {
-        val char = str[i].lowercaseChar()
-        if (result[char] != null) {
-            result[char] = result[char]!! + 1
-        } else {
-            result[char] = 1
-        }
-    }
-    // if char is a number/letter AND a key in object, add one to count
-    // if char is number/letter AND not in object add it to object and set value to 1
-    /// if char is something else (space, period, etc.) don't do anything
-    // return object at end
-    return result
-}
-
-fun main() {
-    println(charCount("Julian McMahon"))
-}
+//fun charCount(str: String): HashMap<Char, Int> {
+//    // make object to return at end
+//    val result: HashMap<Char, Int> = hashMapOf()
+//    // loop over string for each character
+//    for (i in str.indices) {
+//        val char = str[i].lowercaseChar()
+//        if (result[char] != null) {
+//            result[char] = result[char]!! + 1
+//        } else {
+//            result[char] = 1
+//        }
+//    }
+//    // if char is a number/letter AND a key in object, add one to count
+//    // if char is number/letter AND not in object add it to object and set value to 1
+//    /// if char is something else (space, period, etc.) don't do anything
+//    // return object at end
+//    return result
+//}
+//
+//fun main() {
+//    println(charCount("Julian McMahon"))
+//}
 
